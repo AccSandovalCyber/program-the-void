6e8e7c06495a3515ef87935a94f0ce80ade3478d
Ricardo Sandoval <AccSandovalCyber@gmail.com>
2024-07-21 01:09:43 -0700

    Update index.html and app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+# Redirect HTTP to HTTPS
+@app.before_request
+def redirect_to_https():
+    if not request.is_secure and not request.headers.get('X-Forwarded-Proto') == 'https':
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
+
 @app.route('/')
 def home():
-    return "Hello, Heroku!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
