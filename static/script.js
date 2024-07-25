e2961f0c8189c27f03071c4a09072d862b7634f1
Ricardo Sandoval <AccSandovalCyber@gmail.com>
2024-07-24 21:57:00 -0700

    Generator

--- static/script.js
+++ static/script.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Code to run when the DOM is fully loaded
-
-    // Example: Add event listener to the button if needed
-    const tvButton = document.querySelector('.tv-button');
-    if (tvButton) {
-        tvButton.addEventListener('click', () => {
-            console.log('Button clicked!');
-            // Add more interactivity here if needed
-        });
-    }
-
-    // Additional JS code can go here
+document.getElementById('doorButton').addEventListener('click', function() {
+    fetchRandomGif();
 });
+
+function fetchRandomGif() {
+    const apiKey = 'qPKc2S2fpfff7C3cM1mdpKFTzWZZAwUT'; // Your Giphy API key
+    const apiUrl = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=&rating=G`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const gifUrl = data.data.images.original.url;
+            document.getElementById('giphyImage').src = gifUrl;
+        })
+        .catch(error => console.error('Error fetching GIF:', error));
+}
